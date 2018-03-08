5e59591a77f20b5214d9b3ae93df1c1056fca51a
demo <demo@nga>
2018-03-08 15:32:19 +0000

    added price verification

--- src/main/java/net/mf/LeanFtTest.java
+++ src/main/java/net/mf/LeanFtTest.java
@@ -51,6 +51,7 @@ public class LeanFtTest extends UnitTestClassBase {
 
     @Test
     public void buySpeaker() throws GeneralLeanFtException, InterruptedException {
+        
         aosModel.SPEAKERS().highlight();
         aosModel.SPEAKERS().click();
 
